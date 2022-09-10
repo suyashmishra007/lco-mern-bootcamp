259de035a19ca0fe969a2427179c2637d96b3310
Suyash Mishra <58506713+suyashmishra007@users.noreply.github.com>
2022-09-10 22:27:41 +0530

    section 3 completed

---
+++ models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const { createHmac } = require("node:crypto");
+const { Schema } = mongoose;
+const uuidv1 = require("uuid/v1");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userInfo: {
+    type: String,
+    trim: true,
+  },
+  encry_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    // using _ means , we keep _password private.
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
+  },
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
